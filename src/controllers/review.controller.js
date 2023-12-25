8cdc9c11ebe9d8faed67fade462c20801711318a
restuadil <restuadil09@gmail.com>
2023-12-25 18:23:18 +0700

    add jsonwebtoken

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -90,3 +90,22 @@ export const updateReview = async (req, res) => {
         })
     }
 }
+export const deleteReview = async (req, res) => {
+    const id = parseInt(req.params.id)
+    const reviewFromDB = await service("SELECT * FROM reviews WHERE id = ?", [id])
+    if (reviewFromDB.length === 0) return res.status(404).json({ status: false, message: "Review Not Found" })
+    try {
+        await service("DELETE FROM reviews WHERE id = ?", [id])
+        logger.info("Success Delete Review")
+        res.status(200).json({
+            status: true,
+            message: "Success Delete Review"
+        })
+    } catch (error) {
+        logger.error(error.message)
+        res.status(500).json({
+            status: false,
+            message: error.message
+        })
+    }
+}
--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createReview, getAllReviews } from "../controllers/review.controller.js";
+import { createReview, deleteReview, getAllReviews, getReviewById, updateReview } from "../controllers/review.controller.js";
 import { requireUser } from "../middleware/auth.js";
 
 export const ReviewRouter = Router();
 
 
 ReviewRouter.get('/', getAllReviews)
+ReviewRouter.get('/:id', getReviewById)
 ReviewRouter.post('/', requireUser, createReview)
+ReviewRouter.put('/:id', requireUser, updateReview)
+ReviewRouter.post('/:id', requireUser, deleteReview)
